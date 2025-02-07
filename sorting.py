9fec68808c2a4ee3a94e86212273468d16f69d90
Rafael Al-Zohairy <rafael.al-zohairy@elev.ga.ntig.se>
2025-02-07 12:12:57 +0100

    tja

---
+++ sorting.py
@@ -0,0 +1,10 @@
+my_sort = [8, 2, 6, 1] # Använding av sort
+
+my_sort.sort()
+print(f"Användningen av sort: {my_sort}")
+
+
+my_sorting = [8, 2, 6, 1] # Använding av sorted
+
+result = sorted(my_sorting)
+print(f"Användningen av sorted: {result}")
